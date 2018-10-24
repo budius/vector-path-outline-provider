8b0515a3a8e0913b6e9b37558821e006a6d82e66
Ronaldo Pace <ronaldo.pace@sensorberg.com>
2018-10-24 14:11:40 +0200

    added option to build individually from view 0.1.1

--- library/src/main/java/com/sensorberg/vectorpathoutlineprovider/VectorPathOutlineProvider.java
+++ library/src/main/java/com/sensorberg/vectorpathoutlineprovider/VectorPathOutlineProvider.java
@@ -3,17 +3,23 @@ package com.sensorberg.vectorpathoutlineprovider;
 import android.annotation.TargetApi;
 import android.os.Build;
 import android.view.View;
+import android.view.ViewOutlineProvider;
 
 public class VectorPathOutlineProvider {
-	public static void apply(View view, String path, float width, float height) {
-		if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
-			return;
-		}
-		apply_V21(view, path, width, height);
-	}
+    public static void apply(View view, String path, float width, float height) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+            return;
+        }
+        apply_V21(view, path, width, height);
+    }
 
-	@TargetApi(Build.VERSION_CODES.LOLLIPOP)
-	private static void apply_V21(View view, String path, float width, float height) {
-		view.setOutlineProvider(new InternalProvider(path, width, height));
-	}
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    private static void apply_V21(View view, String path, float width, float height) {
+        view.setOutlineProvider(new InternalProvider(path, width, height));
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public static ViewOutlineProvider of(String path, float width, float height) {
+        return new InternalProvider(path, width, height);
+    }
 }
